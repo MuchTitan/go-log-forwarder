1de612e054f407bf571e32e17db35aad69bbabdb
Robin Langhof <robinlanghof@gmail.com>
2024-05-23 12:37:49 +0200

    Works kinda

--- client/main.go
+++ client/main.go
@@ -4,34 +4,66 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"log-forwarder-client/models"
 	"log-forwarder-client/reader"
 	"os"
 	"os/signal"
 	"path/filepath"
+	"slices"
 	"syscall"
 	"time"
-
 )
 
-func getDirContent(path string) ([]string, error) {
-  result := []string{} 
+func CreateFile(parent context.Context,path string) *models.FileState{
+  ctx, cancel := context.WithCancel(parent)
+  return &models.FileState{
+    Path: path,
+    LastSendLine: 0,
+    State: ctx,
+    Cancel: cancel,
+  }
+}
+
+func checkIfFileStateExists(slice []*models.FileState,path string) bool {
+  for _, file := range(slice) {
+    if file.Path == path {
+      return true
+    }
+  }
+  return false
+}
+
+func getDirContent(ctx context.Context,path string) ([]*models.FileState, error) {
+  result := []*models.FileState{} 
   dirContent,err := os.ReadDir(path)
   if err != nil {
     return result,err
   }
   for _,t := range dirContent {
-    fmt.Println(filepath.Join(path,t.Name()))
     if t.IsDir(){
-      tmp,err := getDirContent(filepath.Join(path,t.Name()))
+      tmp,err := getDirContent(ctx,filepath.Join(path,t.Name()))
       if err != nil {
         return result,err
       }
       result = append(result, tmp...)
     }else{
-      result = append(result, filepath.Join(path,t.Name()))
+      if !checkIfFileStateExists(result, filepath.Join(path,t.Name())) {
+        t := CreateFile(ctx,filepath.Join(path,t.Name()))
+        result = append(result, t)
+      }
     }
   }
   return result, nil
+}
+
+func difference[T comparable](slice1, slice2 []T) []T {
+	result := []T{}
+	for _, item1 := range slice1 {
+		if !slices.Contains(slice2, item1) {
+			result = append(result, item1)
+		}
+	}
+	return result
 }
 
 func startReading(ctx context.Context,path string) {
@@ -43,22 +75,63 @@ func startReading(ctx context.Context,path string) {
 	}()
 }
 
+func StopZombieContext(input1 []string, input2 map[string]context.CancelFunc) map[string]context.CancelFunc{
+  result := make(map[string]context.CancelFunc)
+  for key,cancel := range input2 {
+    if !slices.Contains(input1,key) {
+      cancel()
+    }else{
+      result[key]=cancel
+    }
+  }
+  return result
+}
+
+func MapContainsKey[T comparable, E any](input map[T]E, searchTerm T) bool {
+  _, exits := input[searchTerm]
+  return exits
+}
+
 func main() {
 	// Create a context and cancel function to manage the lifecycle of operations
   ctx, cancel := context.WithCancel(context.Background())
 	defer cancel() // Ensure the context is canceled when main exits
+  runningLogFile := make(map[string]context.CancelFunc)
 
-  // startReading(ctx,"./test.log")
-  dirContent, err := getDirContent("./test/")
-  if err != nil {
-    log.Fatalln(err)
+  errChan := make(chan error)
+  go func(){
+    for {
+      dirContent, err := getDirContent(ctx,"./test/")
+      if err != nil {
+        log.Fatalln(err)
+      }
+      // for _ ,x := range(dirContent) {
+      //   log.Println(x.Path)
+      // }
+      stillRunningLogFiles := []string{}
+
+      for _, data := range dirContent {
+        log.Println(runningLogFile,data.Path)
+        if MapContainsKey(runningLogFile,data.Path){
+          if data.State.Err() != nil {
+            errChan <- reader.ReadFile(data.State, data.Path)
+            stillRunningLogFiles = append(stillRunningLogFiles, data.Path)
+          }
+        } else {
+          log.Printf("Start Reading of File %s", data.Path)
+          errChan <- reader.ReadFile(data.State, data.Path)
+          stillRunningLogFiles = append(stillRunningLogFiles, data.Path)
+          runningLogFile[data.Path] = data.Cancel
+        }
+      }
+      runningLogFile = StopZombieContext(stillRunningLogFiles,runningLogFile)
+      time.Sleep(time.Second * 2)
+    }
+  }()
+    
+  for err := range(errChan) {
+    log.Println(err)
   }
-
-  for _, dirData := range dirContent {
-    startReading(ctx, dirData)
-  }
-
-	// Start the ReadFile operation in a separate goroutine
 
 	// Wait for a termination signal (SIGINT or SIGTERM)
 	sigCh := make(chan os.Signal, 1)
--- client/models/models.go
+++ client/models/models.go
@@ -1,11 +1,23 @@
 package models
 
+import (
+  "context"
+)
+
 type LogLine struct {
   Data string
   LineNum int
   TransmitionStatus bool
 }
+
 type LogFile struct {
   Path string
   LogLines *[]LogLine
+}
+
+type FileState struct {
+  Path string
+  LastSendLine int
+  State context.Context
+  Cancel context.CancelFunc 
 }
